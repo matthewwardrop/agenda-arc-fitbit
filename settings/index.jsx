2d36b9b930da712885266e8bcbb7532d0ce04dda
Matthew Wardrop <mpwardrop@gmail.com>
2020-08-23 11:08:48 -0700

    Fix race condition in settings app.

--- settings/index.jsx
+++ settings/index.jsx
@@ -17,28 +17,30 @@ function settingsComponent(props) {
           label="Hide declined events" />
       </Section>
         <Section title="Calendars">
-      {calendars.map(calendar => <Section>
-        <Toggle
-          settingsKey={"cal:" + calendar["title"] + ':enabled'}
-          label={calendar["title"]}
-        />
-        {props.settingsStorage.getItem("cal:" + calendar["title"] + ':enabled') === "true" &&
-          <ColorSelect
-            centered={true}
-            settingsKey={"cal:" + calendar["title"] + ':color'}
-            colors={[
-              {color: 'white'},
-              {color: 'tomato'},
-              {color: 'sandybrown'},
-              {color: 'gold'},
-              {color: 'lightgreen'},
-              {color: 'aquamarine'},
-              {color: 'deepskyblue'},
-              {color: 'plum'}
-            ]}
+        {calendars !== null ? calendars.map(calendar => <Section>
+          <Toggle
+            settingsKey={"cal:" + calendar["title"] + ':enabled'}
+            label={calendar["title"]}
           />
-        }
-      </Section>)}
+          {props.settingsStorage.getItem("cal:" + calendar["title"] + ':enabled') === "true" &&
+            <ColorSelect
+              centered={true}
+              settingsKey={"cal:" + calendar["title"] + ':color'}
+              colors={[
+                {color: 'white'},
+                {color: 'tomato'},
+                {color: 'sandybrown'},
+                {color: 'gold'},
+                {color: 'lightgreen'},
+                {color: 'aquamarine'},
+                {color: 'deepskyblue'},
+                {color: 'plum'}
+              ]}
+            />
+          }
+        </Section>) : <Text bold align="center">
+          Calendars have not yet been imported. This should happen in a few seconds. Please try closing this Settings page, and then re-opening.
+        </Text>}
       </Section>
     </Page>
   );
